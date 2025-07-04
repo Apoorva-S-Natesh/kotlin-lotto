0857ed264f4198e29b0514c1ee6c0df4ac430350
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-04 14:47:08 +0200

    fix(WinningRankTest): remove test dependency

--- src/test/kotlin/lotto/WinningRankTest.kt
+++ src/test/kotlin/lotto/WinningRankTest.kt
@@ -1,36 +1,36 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class WinningRankTest {
     @Test
     fun `return FIRST when 6 numbers match`() {
         val rank = Rank.valueOf(6, false)
-        assertEquals(rank, Rank.FIRST)
+        assertThat(rank).isEqualTo(Rank.FIRST)
     }
 
     @Test
     fun `return SECOND when 5 numbers match and bonus`() {
         val rank = Rank.valueOf(5, true)
-        assertEquals(rank, Rank.SECOND)
+        assertThat(rank).isEqualTo(Rank.SECOND)
     }
 
     @Test
     fun `return SECOND when 5 numbers match without bonus`() {
         val rank = Rank.valueOf(5, false)
-        assertEquals(rank, Rank.THIRD)
+        assertThat(rank).isEqualTo(Rank.THIRD)
     }
 
     @Test
     fun `return THIRD when 4 numbers match`() {
         val rank = Rank.valueOf(4, false)
-        assertEquals(rank, Rank.FOURTH)
+        assertThat(rank).isEqualTo(Rank.FOURTH)
     }
 
     @Test
     fun `return FOURTH when 3 numbers match`() {
         val rank = Rank.valueOf(3, false)
-        assertEquals(rank, Rank.FIFTH)
+        assertThat(rank).isEqualTo(Rank.FIFTH)
     }
 }
