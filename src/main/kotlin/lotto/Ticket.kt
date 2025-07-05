460bf9a31e14a0fabba2e76d47e4937cec6c9a2a
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-05 12:29:52 +0200

    test(TicketTest): refactoring ticket tests

--- src/main/kotlin/lotto/Ticket.kt
+++ src/main/kotlin/lotto/Ticket.kt
@@ -1,9 +1,14 @@
 package lotto
 
+import kotlin.coroutines.Continuation
+
 class Ticket(val numbers: List<Int>) {
     init {
         require(
-            numbers.size == Constants.NUMBER_COUNT && numbers.distinct().size == Constants.NUMBER_COUNT,
+            numbers.size == Constants.NUMBER_COUNT,
+        ) { "Need ${Constants.NUMBER_COUNT} numbers" }
+        require(
+            numbers.distinct().size == Constants.NUMBER_COUNT
         ) { "Numbers have to be distinct" }
         require(numbers.all { it in Constants.MINIMUM_NUMBER..Constants.MAXIMUM_NUMBER }) {
             "Numbers should be between ${Constants.MINIMUM_NUMBER} to ${Constants.MAXIMUM_NUMBER}" }
--- src/test/kotlin/lotto/TicketTest.kt
+++ src/test/kotlin/lotto/TicketTest.kt
@@ -1,63 +1,64 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class TicketTest {
     @Test
-    fun `size ticket numbers are not 6`() {
-        assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(listOf(10, 2, 34, 5, 2, 15, 6))
-            require(ticket.numbers.size == Constants.NUMBER_COUNT)
+    fun `ticket numbers are not equal to 6 throws exception`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            Ticket(listOf(10, 2, 34, 5, 2, 15, 6))
         }
+        assertThat(exception.message).isEqualTo("Need ${Constants.NUMBER_COUNT} numbers")
     }
 
     @Test
-    fun `size ticket numbers is 6`() {
-        assertDoesNotThrow {
-            val ticket = Ticket(listOf(1, 2, 34, 6, 45, 41))
-            require(ticket.numbers.size == Constants.NUMBER_COUNT)
+    fun `ticket numbers are equal to 6 does not throw exception`() {
+        val ticket = assertDoesNotThrow {
+            Ticket(listOf(1, 2, 34, 6, 45, 41))
         }
+        assertThat(ticket.numbers).hasSize(Constants.NUMBER_COUNT)
     }
 
     @Test
-    fun `ticket numbers are between 1 and 45`() {
-        assertDoesNotThrow {
-            val ticket = Ticket(listOf(1, 2, 34, 6, 45, 41))
-            require(ticket.numbers.all { it in Constants.MINIMUM_NUMBER..Constants.MAXIMUM_NUMBER })
+    fun `ticket numbers not in the range throws exception`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            Ticket(listOf(1, 2, 34, 46, 4, 41))
         }
+        assertThat(exception.message).isEqualTo("Numbers should be between ${Constants.MINIMUM_NUMBER} to ${Constants.MAXIMUM_NUMBER}")
     }
 
     @Test
-    fun `ticket numbers are not between 1 and 45`() {
-        assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(listOf(1, 2, 34, 46, 4, 41))
-            require(ticket.numbers.all { it in Constants.MINIMUM_NUMBER..Constants.MAXIMUM_NUMBER })
+    fun `ticket numbers in the range does not throw exception`() {
+        val ticket =  assertDoesNotThrow {
+            Ticket(listOf(1, 2, 34, 6, 45, 41))
         }
+        assertThat(ticket.numbers).allMatch { it in Constants.MINIMUM_NUMBER..Constants.MAXIMUM_NUMBER }
     }
 
     @Test
-    fun `ticket numbers are duplicate`() {
-        assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(listOf(1, 1, 2, 3, 4, 5))
-            require(ticket.numbers.size == ticket.numbers.distinct().size)
+    fun `ticket numbers are duplicate throws exception`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            Ticket(listOf(1, 1, 2, 3, 4, 5))
         }
+        assertThat(exception.message).isEqualTo("Numbers have to be distinct")
     }
 
     @Test
-    fun `ticket numbers are not duplicate`() {
-        assertDoesNotThrow {
-            val ticket = Ticket(listOf(1, 10, 2, 3, 4, 5))
-            require(ticket.numbers.size == ticket.numbers.distinct().size)
+    fun `ticket numbers are not duplicate does not throw exception`() {
+        val ticket = assertDoesNotThrow {
+            Ticket(listOf(1, 10, 2, 3, 4, 5))
         }
+        assertThat(ticket.numbers.size).isEqualTo(ticket.numbers.distinct().size)
     }
 
     @Test
     fun `ticket numbers should not be empty`() {
-        assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(emptyList())
-            require(ticket.numbers.size >= 0)
+        val exception =  assertThrows<IllegalArgumentException> {
+            Ticket(emptyList())
         }
+        assertThat(exception.message).isEqualTo("Need ${Constants.NUMBER_COUNT} numbers" )
     }
 }
